f368c7916ecac519d91210e09d5b880999348e45
Everton Rufino <everton7ej@gmail.com>
2022-04-20 03:09:15 -0300

    Project Uploaded

--- SpringBack Old/src/main/java/com/heyiamej/project/exception/AtividadeNaoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package com.heyiamej.project.exception;
-
-public class AtividadeNaoEncontradaException extends Exception {
-    public AtividadeNaoEncontradaException(Long id) {
-        super(String.format("Atividade com id %d não encontrada!", id));
-    }
-}
--- SpringBack Old/src/main/java/com/heyiamej/project/exception/ProfissaoNaoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package com.heyiamej.project.exception;
-
-public class ProfissaoNaoEncontradaException extends Exception {
-    public ProfissaoNaoEncontradaException(Long id) {
-        super(String.format("Profissão com id %d não encontrada!", id));
-    }
-}
